669f9e6f242fc5708dc50602fc9912da01e70904
Danil Poletavkin <poletavkin.danil@constructor.ru>
2026-06-21 11:43:06 +0500

    day7_reports -> day7_messages

--- day7.py
+++ day7.py
@@ -7,8 +7,8 @@
 # What is module? Module is just simply python file. So look at the explorer - all "day...py" files are modules
 # Module is imported by using "import" keyword. For example let's create simple module and import it
 
-import day7_reports # Here we imported module ie another python file into our program
+import day7_messages # Here we imported module ie another python file into our program
 
-day7_reports.print_m('Hello!')
+day7_messages.print_hello()
 
 # TASK: create your own module, then import it and execute the program
