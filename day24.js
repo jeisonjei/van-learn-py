9b03bd538580f847a2eb8a9bcab0c3baa6940af7
Danil Poletavkin <poletavkin.danil@constructor.ru>
2026-07-13 20:20:31 +0500

    day24 added

---
+++ day24.js
@@ -0,0 +1,41 @@
+/** #-------------------------- #
+    # ######### DAY 24 ######## #
+    # ------------------------- # */
+
+/**
+ * Today we'll learn what are `undefined` and `null` types in JavaScript,
+ * then we'll repeat some you've already learned
+ */
+
+/**
+ * You already know `string`, `number`, `boolean` JS types - these are called "primitive" types
+ * Also you know `object` and `array` types which are called "non-primitive" types
+ * And there are two special primitive types in JavaScript: `null` and `undefined`
+ */
+
+/**
+ * If you're declaring variable and not initializing it with some value, it's type by default will be `undefined`
+ */
+
+let a;
+typeof a; // undefined
+
+/**
+ * So `undefined` type is default type of unassigned variables. Also there
+ * is another special `null` type. By default variables are never can be `null` - `null` type can be only assigned to variable intentionally
+ */
+
+let a = null;
+typeof a; // null
+
+/**
+ * TASK0: Think about these two new types you've just learned. In which cases `null` type is being used you think?
+ */
+
+/** TASK1: Create minimal simpliest html page with header and button. Button should have "click" event listener. 
+ * On click header should change it's text
+*/
+
+/**
+ * Create variables (just variables) with JS types you know and check their types
+ */
