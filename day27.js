219bcd933e9cccf7cbcfd9eeb2068a94bdd18736
Danil Poletavkin <jeisonj2019@gmail.com>
2026-07-19 20:30:03 +0500

    day27 added

---
+++ day27.js
@@ -0,0 +1,37 @@
+/** #-------------------------- #
+    # ######### DAY 27 ######## #
+    # ------------------------- # */
+
+/**
+ * Today you'll see real javascript app and will make some fixes
+ */
+
+/**
+ * Clone https://github.com/jeisonjei/when-we-go-to-velikij-ustug repository to your computer and open it.
+ * See? It contains html file, css file and `main.js` file where most of JS code is located.
+ * Also you'll see `storageService.js` file which is responsible on saving data to localStorage.
+ */
+
+/**
+ * Take a look on `main.js` file. Try to imagine how this code is executed. This JavaScript file is
+ * executed line by line from top to bottom. Expressions on the outer scope executed immediatelly while functions just defined
+ * and executed when them are called.
+ */
+
+/**
+ * TASK0: Go through `main.js` file and notice pieces of code familiar to you. What expression is executed very first?
+ */
+
+/**
+ * TASK1: Take a look on html and css files. Try to imagine how this html page looks like in the browser not running the html yet.
+ */
+
+/**
+ * Now you can run this project. Just simply run `index.html` as we usually did
+ */
+
+/**
+ * TASK2: Find input element where tasks are being written (Hint: use DevTools 'Elements' tab). And implement focus on this input field when page is loaded
+ * Hints: use document.addEventListener('load', ...some arrow function callback...) to catch page loading,
+ * then get this input element and use `element.focus()` function to focus on it
+ */
