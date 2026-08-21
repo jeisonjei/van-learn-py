066d59d47b4d7af845b3be336ea62106649fe9b6
Danil Poletavkin <jeisonj2019@gmail.com>
2026-08-21 19:49:09 +0500

    day33 added

---
+++ day33.js
@@ -0,0 +1,20 @@
+/** #-------------------------- #
+    # ######### DAY 33 ######## #
+    # ------------------------- # */
+
+/**
+ * Today is a 'task day'. Let's review what are functions in JavaScript. Complete all tasks yourself, don't use AI.
+ * Also it's useful to start following 'HEAT FIRST' principle when you're write the code: FIRST - THINK, THEN - DO.
+ */
+
+/**
+ * TASK0: Write function to sum 2 numbers and return result. Test it in NODE JS or in the browser console
+ */
+
+/**
+ * TASK1: Create a function which will add 'Remember this' to any text string it receives. 
+ */
+
+/**
+ * TASK2: Create one more function to get an array of numbers and return another array of squares of those numbers
+ */
