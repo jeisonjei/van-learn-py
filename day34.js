c44a0e0aebaede54c9b23a8cc66c8802a3a23155
Danil Poletavkin <jeisonj2019@gmail.com>
2026-09-24 19:00:12 +0500

    day34 added

---
+++ day34.js
@@ -0,0 +1,27 @@
+/********************
+ ******* DAY34 ******
+ ********************/
+
+/**
+ * Today, in the AI age, programming is not magic anymore - this is a fact. Before that, only programmers - strange and smart guys - could read and understand
+ * these mysterious symbols that we call 'code'. Now - nothing changes, with only one difference - anyone can ask
+ * AI to create a program or website and it (he or she or it ??!) will do it in a few minutes.
+ * 
+ * Nevertheless, programming will remain a serious human activity, and real programmers will always have to be able
+ * to read and write code MANUALLY despite the power of AI. 
+ * And to use AI effectively, it's better to be a real programmer.
+ * 
+ * So, let's return to JS basics. Tasks for today:
+ */
+
+/**
+ * TASK0: Create a simple JS object car. Assign properties to it that a real car usually has.
+ */
+
+/**
+ * TASK1: List all your newly created object properties using a loop. What loop - I hope you'll remember.
+ */
+
+/** 
+ * TASK2: Create a function that takes an object as an argument and prints all its properties values to the console.
+ */
